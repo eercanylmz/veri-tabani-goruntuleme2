a0f3932f410c01946a93ca949a712b1fd0bf99db
ERCAN <yilmazercan404@gmail.com>
2022-12-01 01:52:08 +0300

    Proje dosyası ekle.

---
+++ veri tabanı goruntuleme2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace veri_tabanı_goruntuleme2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1148, 222);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "AdSoyad";
+            this.columnHeader1.Width = 173;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Sınıf";
+            this.columnHeader2.Width = 183;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Okul";
+            this.columnHeader3.Width = 187;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Şehir";
+            this.columnHeader4.Width = 193;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(570, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "VIEW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "CİNSİYET";
+            this.columnHeader5.Width = 122;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1355, 641);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
+
---
+++ veri tabanı goruntuleme2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace veri_tabanı_goruntuleme2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connect = new SqlConnection("Data Source=ERCANMONSTER\\ERCANSERVER;Initial Catalog=SORU;Integrated Security=True");
+        private void goster()
+        {
+            connect.Open();
+            SqlCommand komut = new SqlCommand("Select * from OgrencıKayıt", connect);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["AdSoyad"].ToString();
+                ekle.SubItems.Add (oku["Sınıf"].ToString());
+                ekle.SubItems.Add(oku["Okul"].ToString());
+                ekle.SubItems.Add(oku["Şehir"].ToString());
+                ekle.SubItems.Add(oku["CINSIYET"].ToString());
+                listView1.Items.Add(ekle);
+            }
+            connect.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            goster();
+        }
+    }
+}
